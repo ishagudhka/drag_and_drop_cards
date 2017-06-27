e8bcd04da00ee1cf4549fd44644623443a5d0ec5
Renee Char <reneechar@Renees-MacBook-Pro.local>
2017-06-26 22:18:42 -0700

    follows tutorial to get drag and drop working

---
+++ Assets/Draggable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	Transform parentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log ("OnBeginDrag");
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent ( this.transform.parent.parent );
+	}
+
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log ("OnDrag");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log ("OnEndDrag");
+		this.transform.SetParent (parentToReturnTo);
+	}
+}
